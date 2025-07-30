6a6b847c24f7d86efd3cf41ac9acb9bbb1e7c60c
Medeter <155871806+Medeter@users.noreply.github.com>
2025-07-30 19:57:42 +0700

    Initial commit

---
+++ src/main/java/com/example/Homework_Helloworld/model/MessageResponse.java
@@ -0,0 +1,29 @@
+package com.example.Homework_Helloworld.model;
+
+public class MessageResponse {
+    private String message;
+    private String status;
+    
+    public MessageResponse() {}
+    
+    public MessageResponse(String message, String status) {
+        this.message = message;
+        this.status = status;
+    }
+    
+    public String getMessage() {
+        return message;
+    }
+    
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+    public String getStatus() {
+        return status;
+    }
+    
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/com/example/Homework_Helloworld/service/MessageService.java
@@ -0,0 +1,11 @@
+package com.example.Homework_Helloworld.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageService {
+
+    public String getWelcomeMessage() {
+        return "Hello_Kumpee_Kongsuk!!";
+    }
+}
